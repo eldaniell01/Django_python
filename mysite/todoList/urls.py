7ba35a84c35389f353ba86584b2df3ab31816fc0
eldaniell01 <amontepequem@miumg.edu.gt>
2023-11-25 19:47:18 -0600

    lista de tarea

--- mysite/todoList/urls.py
+++ mysite/todoList/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, Register
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
@@ -7,6 +8,8 @@ urlpatterns = [
     path('task_create/', TaskCreate.as_view(), name='task_create'),
     path('task_update/<int:pk>', TaskUpdate.as_view(), name='task_update'),
     path('task_delete/<int:pk>', TaskDelete.as_view(), name='task_delete'),
-    path('login/', CustomLoginView.as_view(), name='login')
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', Register.as_view(), name='register')
 ]
 
--- mysite/todoList/views.py
+++ mysite/todoList/views.py
@@ -1,32 +1,53 @@
-from django.shortcuts import render
+from typing import Any
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from .models import Task
 # Create your views here.
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
-    context_object_name = 'Task'
+    context_object_name = 'tasks'
     
-class TaskDetail(DetailView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(state=False).count()
+        search = self.request.GET.get('search') or ''
+        if search:
+            context['tasks'] = context['tasks'].filter(title__icontains=search)
+            
+        context['search'] = search
+        return context
+
+    
+class TaskDetail(LoginRequiredMixin, DetailView):
     model = Task
     context_object_name = 'Task'
     template_name = 'todoList/task_detail.html'
     
-class TaskCreate(CreateView):
+class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'state']
     success_url = reverse_lazy('tasks')
     
-class TaskUpdate(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user 
+        return super(TaskCreate, self).form_valid(form)
+    
+class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
-    fields = '__all__'
+    fields = ['title', 'description', 'state']
     success_url = reverse_lazy('tasks')
     
-class TaskDelete(DeleteView):
+class TaskDelete(LoginRequiredMixin, DeleteView):
     model = Task
     context_object_name= 'task'
     success_url = reverse_lazy('tasks')
@@ -38,3 +59,21 @@ class CustomLoginView(LoginView):
     redirect_authenticated_user = True
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+class Register(FormView):
+    template_name = 'todoList/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+    
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(Register, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(Register, self).get(*args, **kwargs)
+        
