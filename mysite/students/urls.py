31a19c7064668199b4578b877967045a5cba5ce4
eldaniell01 <amontepequem@miumg.edu.gt>
2023-12-04 19:08:34 -0600

    eliminar y actualizar estudiantes

--- mysite/students/urls.py
+++ mysite/students/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('student/<int:id>', views.view_student, name='view_student'),
-    path('Regristrar/', views.add_student, name='add')
+    path('register/', views.add_student, name='add'),
+    path('edit/<int:id>', views.edit_student, name='edit'),
+    path('delete/<int:id>', views.delete, name='delete')
 ]
