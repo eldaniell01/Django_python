b9348f23969e1b18987d08c52af637afc460a870
eldaniell01 <amontepequem@miumg.edu.gt>
2023-12-10 14:13:00 -0600

    search

--- mysite/items/urls.py
+++ mysite/items/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 app_name= 'item'
 urlpatterns = [
+    path('', views.items, name='items'),
     path('<int:pk>/', views.detail, name='detail'),
     path('new_item/', views.new, name='new_item'),
-    path('delete/<int:pk>', views.delete, name='delete')
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    
 ]
