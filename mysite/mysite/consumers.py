a25fc4ac92af73fa507a5e9ca5dfdedfa7394c0e
eldaniell01 <amontepequem@miumg.edu.gt>
2023-12-21 19:22:09 -0600

    chat terminado

--- mysite/mysite/consumers.py
+++ mysite/mysite/consumers.py
@@ -1,6 +1,8 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+from room.models import Message, Room
+from django.contrib.auth.models import User
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -17,7 +19,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             self.room_group_name, 
-            self.channel_layer
+            self.channel_name
         )
         
     async def receive(self, text_data):
@@ -25,6 +27,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         message = data['message']
         username = data['username']
         room = data['room']
+        
+        await self.save_message(username, room, message)
+        
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -45,4 +50,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'room': room
         }))
         
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+        Message.objects.create(user=user, room=room, content=message)
     
--- mysite/room/views.py
+++ mysite/room/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .models import Room
+from .models import Room, Message
 # Create your views here.
 
 @login_required
@@ -12,4 +12,5 @@ def rooms(request):
 @login_required
 def detail_room(request, slug):
     room = Room.objects.get(slug=slug)
-    return render(request, 'room/room.html', {'room':room})
+    messages = Message.objects.filter(room=room)[0:25]
+    return render(request, 'room/room.html', {'room':room, 'messages':messages})
